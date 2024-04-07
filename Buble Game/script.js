22cd69617333db3eae4ee527d315facc30c3de56
julhasur <shuvochowdhury14@gmail.com>
2024-04-07 16:34:22 -0400

    game created,final commit

--- Buble Game/script.js
+++ Buble Game/script.js
@@ -1,13 +1,21 @@
+var timer=6;// for run timmer function
+var score=0;//to increase store
+var hitrn=0;
+function increaseScore(){
+   score += 10; 
+   document.querySelector("#scoreval").textContent=score;
+}
+
 function makeBubble(){
     var clutter="";
 for(var i=1;i<=175;i++){
    var rn=Math.floor(Math.random()*10) 
 clutter +=`<div class="bubble">${rn}</div>`;
+
 }
 // selecting pannel bottom
 document.querySelector("#pbottom").innerHTML=clutter; 
 }
-var timer=60;
 function runtimer(){
 var counter=setInterval(function(){
    if(timer>0){
@@ -16,14 +24,26 @@ var counter=setInterval(function(){
    }
    else{
     clearInterval(counter);
-   }
+    document.querySelector("#pbottom").innerHTML=`<h1>Game Over!<h1>`;
+  console.log("ended");
+}
 },1000);
 }
 function getnewHit(){
-    var rn=Math.floor(Math.random()*10);
-    document.querySelector("#hitval").innerHTML=rn;
+    hitrn=Math.floor(Math.random()*10);
+    document.querySelector("#hitval").innerHTML=hitrn;
 }
-
-runtimer();
+//adding bubble event listener
+document.querySelector("#pbottom").addEventListener("click",function(dets){
+    var clickednumber=Number(dets.target.textContent);
+    if(clickednumber===hitrn){
+        increaseScore();
+     
+        makeBubble();
+        getnewHit();
+        
+    }
+});
 makeBubble();
+runtimer();
 getnewHit();
